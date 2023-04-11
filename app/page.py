a3722b52df842c6ee40f0b176e01e4f78660e30b
Eduardo Matos <eduardo-matos004@hotmail.com>
2023-04-11 13:52:11 -0300

    feat: adding predict model metrics

--- app/page.py
+++ app/page.py
@@ -3,6 +3,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 from app.database.dataframe import create_dataframe
+from app.services.linear_regression import predict_model
 
 st.set_option("deprecation.showPyplotGlobalUse", False)
 
@@ -16,3 +17,14 @@ def load_page():
 
     plt.title("Relação temperatura x umidade")
     st.pyplot()
+
+    plt.figure(figsize=(10, 10))
+    sns.heatmap(df.corr(), annot=True)
+    plt.title("Correlação Dados")
+    st.pyplot()
+
+    X_test, y_test, y_pred, r2, mse, mae = predict_model(df)
+
+    st.text(f"Coeficiente de Determinação (R²): {r2}")
+    st.text(f"Erro Quadrático Médio (MSE): {mse}")
+    st.text(f"Erro Absoluto Médio (MAE): {mae}")
